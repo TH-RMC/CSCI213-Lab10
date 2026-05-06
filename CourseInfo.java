53c496fba58750e8572f12ce71adc4a8480debc7
Trace Harless <trinityharless@go.rmc.edu>
2026-05-06 15:52:13 -0400

    Created CourseInfo.java and README.md

---
+++ CourseInfo.java
@@ -0,0 +1,9 @@
+public static void main (String[] args) {
+	System.out.println("Course Name: Object-Oriented Programming\n");
+	System.out.println("Instructor: Dr. Elouni\n");
+	System.out.println("Semester: Spring 2026\n");
+	System.out.println("Credits: 4\n");
+	System.out.println("Classroom: COP 204\n");
+	System.out.println("Class Time: MW 1:10PM - 2:40PM // TR 1:20PM - 2:50PM\n");
+	System.out.println("Number of Students: 23\n");
+}
